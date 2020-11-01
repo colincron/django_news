c28debb94d89586aac06a880f615ade6b84a4aab
Colin <colint.cron@gmail.com>
2020-11-01 17:01:20 -0500

    temp doesn't work yet

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+import requests
 
 # Create your views here.
 
+def get_weather():
+        r = requests.get("https://api.openweathermap.org/data/2.5/onecall?lat=27.7920&lon=82.7239&exclude=minutely,hourly,daily,alerts&units=imperial&appid=c587510ed59d1b940a70fea012677a36")
+        data = r.json()
+        temp = data['current']['temp']
+        return temp
+
 class HomePageView(TemplateView):
     template_name = 'home.html'
+    current_weather = get_weather()
