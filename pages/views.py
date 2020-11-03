5573a55537d4f68d4e374401fa342f0ecc35ad69
Colin <colint.cron@gmail.com>
2020-11-02 20:04:50 -0500

    modified css

--- pages/views.py
+++ pages/views.py
@@ -8,7 +8,7 @@ def get_weather():
         r = requests.get("https://api.openweathermap.org/data/2.5/onecall?lat=27.7920&lon=82.7239&exclude=minutely,hourly,daily,alerts&units=imperial&appid=c587510ed59d1b940a70fea012677a36")
         data = r.json()
         temp = data['current']['temp']
-        return temp
+        return str(temp)
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
