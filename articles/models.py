5b41340a0a34fee3b9f83a05377b183aeb75cfad
Colin <colint.cron@gmail.com>
2020-11-05 22:56:18 -0500

    added comments

--- articles/models.py
+++ articles/models.py
@@ -6,6 +6,7 @@ from django.urls import reverse
 # Create your models here.
 
 class Article(models.Model):
+
     title = models.CharField(max_length=255)
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
@@ -21,7 +22,12 @@ class Article(models.Model):
         return reverse('article_detail', args=[str(self.id)])
 
 class Comment(models.Model):
-    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+
+    article = models.ForeignKey(
+    Article,
+    on_delete=models.CASCADE,
+    related_name='comments',
+    )
     comment = models.CharField(max_length=140)
     author = models.ForeignKey(
         get_user_model(),
