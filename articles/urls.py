8557c500cd58425dd15c4a62c341a3fcea285053
Colin <colint.cron@gmail.com>
2020-11-05 22:37:59 -0500

    added authorization

--- articles/urls.py
+++ articles/urls.py
@@ -3,9 +3,12 @@ from .views import (
     ArticleListView, 
     ArticleUpdateView, 
     ArticleDetailView, 
-    ArticleDeleteView)
+    ArticleDeleteView,
+    ArticleCreateView,
+    )
 
 urlpatterns = [
+    path('new/', ArticleCreateView.as_view(), name='article_new'),
     path('<int:pk>/edit/', ArticleUpdateView.as_view(), name='article_edit'),
     path('<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
     path('<int:pk>/delete/', ArticleDeleteView.as_view(), name='article_delete'),
